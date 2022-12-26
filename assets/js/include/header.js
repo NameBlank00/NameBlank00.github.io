6545b5731d826571a91bb2a1fce194c1eec3098a
NameBlank00 <jonathangonzales1357@gmail.com>
2022-12-25 23:21:15 -0600

    Update header.js

--- assets/js/include/header.js
+++ assets/js/include/header.js
@@ -3,10 +3,6 @@ document.write('\
 <!-- Header -->\
 	<header id="header">\
 			<ul class="icons">\
-				<li><a href="#" class="icon brands fa-twitter"><span class="label">Twitter</span></a></li>\
-				<li><a href="#" class="icon brands fa-facebook-f"><span class="label">Facebook</span></a></li>\
-				<li><a href="#" class="icon brands fa-snapchat-ghost"><span class="label">Snapchat</span></a></li>\
-				<li><a href="#" class="icon brands fa-instagram"><span class="label">Instagram</span></a></li>\
 				<li><button onclick="myFunction()">light mode?</button>\
 					</li>\
 			</ul>\
