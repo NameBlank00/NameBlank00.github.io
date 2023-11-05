0f5579316d02a8efa4d9e7c05befb1b0639b3858
NameBlank00 <jonathangonzales1357@gmail.com>
2023-11-04 23:11:10 -0500

    .

--- assets/js/include/sidebar.js
+++ assets/js/include/sidebar.js
@@ -62,10 +62,11 @@ document.write('\
 						</ul>\
 					</section>\
 \
-				<!-- Footer -->\
+				<!-- <!-- Footer -->\
 					<footer id="footer">\
 						<p class="copyright">&copy; Untitled. All rights reserved. Demo Images: <a href="https://unsplash.com">Unsplash</a>. Design: <a href="https://html5up.net">HTML5 UP</a>.</p>\
 					</footer>\
+					-->\
 \
 			</div>\
 		</div>\
