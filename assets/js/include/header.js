4b32166da2ccc16a6f683169b371118daea8500c
NameBlank00 <jonathangonzales1357@gmail.com>
2023-11-17 22:09:56 -0600

    Update header.js

--- assets/js/include/header.js
+++ assets/js/include/header.js
@@ -1,25 +1,18 @@
 document.write('\
 \
-<script>\
-	var element = document.body;\
-	document.getElementById("DarkModeButton").addEventListener("click", function (e) {\
-	document.body.classList.toggle("dark-mode");\
-	// element.classList.toggle("dark-mode");\
-	if (e.target.textContent === "Light Mode") {\
-		e.target.textContent = "Dark Mode";\
-	} else {\
-		e.target.textContent = "Light Mode";\
-	}\
-	});\
-};\
-</script>\
 <!-- Header -->\
-<header id="header">\
-	<ul class="icons">\
-		<li> <button id="DarkModeButton">Light Mode</button>\
-			</li>\
-	</ul>\
-</header>\
-\
+	<header id="header">\
+			<ul class="icons">\
+				<li><button onclick="myFunction()">light mode?</button>\
+					</li>\
+			</ul>\
+	</header>\
+	\
+	<script>\
+		function myFunction() {\
+		   var element = document.body;\
+		   element.classList.toggle("dark-mode");\
+		}\
+		</script>\
 \
 ');
