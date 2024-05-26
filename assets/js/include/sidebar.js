ee4136e490c886243007e8af9ce80fcd8f9a8954
NameBlank00 <jonathangonzales1357@gmail.com>
2024-05-26 00:24:00 -0500

    0

--- assets/js/include/sidebar.js
+++ assets/js/include/sidebar.js
@@ -1,16 +1,6 @@
 
 document.write('\
 \
-				<!-- Search -->\
-					<section id="search" class="alt">\
-						<form role="search" id="form">\
-							<input type="search" name="q" id="query" placeholder="Search" />\
-								</form>\
-\
-                			<script src="https://nameblank00.github.io/assets/js/search.js"></script>\
-\
-					</section>\
-\
 				<!-- Menu -->\
 					<nav id="menu">\
 						<header class="major">\
